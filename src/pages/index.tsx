19b96138d5f4293a5a8a7900de4acebd8c84948b
WeslleySOR <weslleywruas@gmail.com>
2022-08-21 21:39:11 -0300

    Project Ready to Start

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>MKS Sistemas</title>
+        <meta name="description" content="Next-app website developed to challenge" />
+      </Head>
+      <main>
+        <h1>Home</h1>
+      </main>
+    </div>
+  );
+}
